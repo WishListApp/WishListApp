bd387bdf2c16546042ff75134da08a59930a55d5
Vladislavs Zimnikovs <zimnikovvladislav@gmail.com>
2020-03-25 16:34:38 +0200

    Balance added

---
+++ src/main/java/com/wlt/wla/auth/model/Balance.java
@@ -0,0 +1,13 @@
+package com.wlt.wla.auth.model;
+
+public class Balance {
+    public float getBalanceChange() {
+        return balanceChange;
+    }
+
+    public void setBalanceChange(float balanceChange) {
+        this.balanceChange = balanceChange;
+    }
+
+    private float balanceChange;
+}

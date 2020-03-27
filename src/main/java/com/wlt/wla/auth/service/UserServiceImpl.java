2a9130f42676cec0f89c0a785f7e5ea7dc17a5fd
Maxims Samsonovs <maxims.samsonovs@example.com>
2020-03-27 12:57:27 +0200

    Admin Role added

--- src/main/java/com/wlt/wla/auth/service/UserServiceImpl.java
+++ src/main/java/com/wlt/wla/auth/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wlt.wla.auth.service;
 
+import com.wlt.wla.auth.model.Role;
 import com.wlt.wla.auth.model.User;
 import com.wlt.wla.auth.repository.RoleRepository;
 import com.wlt.wla.auth.repository.UserRepository;
@@ -8,6 +9,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,7 +24,17 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-        user.setRoles(new HashSet<>(roleRepository.findAll()));
+        //user.setRoles(new HashSet<>(roleRepository.findById(1l).orElse()));
+        Role myRole = new Role();
+        myRole.setId(1l);
+        myRole.setName("User");
+        Set<Role> myRoles = new HashSet<Role>();
+        myRoles.add(myRole);
+		user.setRoles(myRoles);
+        //user.setRoles(roleRepository.findById(1l).orElse(null));
+        //Foo foo = roleRepository.findById(1l).orElse(null);
+        //roleRepository.find
+        //user.setRoles(roles);
         userRepository.save(user);
     }
 
